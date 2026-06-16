96306969424af558d724ce71b6403e08bc626828
MagullOff <kacprzak.dev@gmail.com>
2026-06-16 19:07:01 +0200

    homepage

--- modules/features/homelab/homepage.nix
+++ modules/features/homelab/homepage.nix
@@ -38,9 +38,6 @@
 
       settings = {
         title = "Dashboard";
-        background = {
-          image = "https://media.sketchfab.com/models/57bf206367214ff48d20e179c539e953/thumbnails/52fa37b473e74db190ea64c2108a607f/375663e0738d4a7985ed5ba9e4dc5f83.jpeg"; 
-        };
       };
     };
 
