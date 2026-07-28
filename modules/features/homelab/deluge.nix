013d10e2c782f6f72b2359fa4e17f57b7e2e3f68
MagullOff <kacprzak.dev@gmail.com>
2026-07-28 16:14:43 +0200

    arrrr

--- modules/features/homelab/deluge.nix
+++ modules/features/homelab/deluge.nix
@@ -2,7 +2,12 @@
   flake.nixosModules.deluge = { config, pkgs, ... }: {
     services.deluge = {
       enable = true;
+      web.enable = true;
+      web.port = 8112;
       openFirewall = true; 
     };
+
+    networking.firewall.allowedTCPPorts = [ 6881 ];
+    networking.firewall.allowedUDPPorts = [ 6881 ];
   };
 }
