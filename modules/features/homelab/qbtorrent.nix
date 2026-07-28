5aa22c36a3ea5e049c21d102c64e557577790fdd
MagullOff <kacprzak.dev@gmail.com>
2026-07-28 14:46:26 +0200

    arr

--- modules/features/homelab/qbtorrent.nix
+++ modules/features/homelab/qbtorrent.nix
@@ -5,5 +5,7 @@
       openFirewall = true; # Opens the webuiPort and torrentingPort in systemd firewall
       webuiPort = 8087;    # Default is 8080
     };
+    
+    networking.firewall.allowedTCPPorts = [ 8087 ];
   };
 }
